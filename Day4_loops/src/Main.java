9e06eb2a1682a68a291c13c3f2979d659b85bf08
vitalyf2024 <162030022+vitalyf2024@users.noreply.github.com>
2024-03-11 19:17:43 +0200

    Task1 added
    Added Task1 for Day4 (March 11)

--- Day4_loops/src/Main.java
+++ Day4_loops/src/Main.java
@@ -1,14 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        sample1();
+        task1();
     }
 
-    public static void sample1(){
+    public static void task1(){
 
-        Scanner sc String b = "aaa";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ievadiet simbolu virkni:");
 
-        if(!b.equals("aaa")){
+        String input = sc.nextLine();
 
+        int input_len = input.length();
 
+        if(input.equals("programmesana")) {
+            System.out.println("Uzvarēji!");
+        } else if(input_len == 13){
+                System.out.println("vienāds simbolu skaits");
+            } else {
+            System.out.println("Zaudēji");
         }
-}
+        }
+    }
