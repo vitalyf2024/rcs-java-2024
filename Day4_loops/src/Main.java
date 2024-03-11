71ce4fd9479ca1d79adb918f0f527e6aa0c84f0a
vitalyf2024 <162030022+vitalyf2024@users.noreply.github.com>
2024-03-11 18:59:39 +0200

    New folder
    New folder added

---
+++ Day4_loops/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        sample1();
+    }
+
+    public static void sample1(){
+
+        Scanner sc String b = "aaa";
+
+        if(!b.equals("aaa")){
+
+
+        }
+}
