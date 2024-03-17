2cf83951b2c19c89b9fbda398b80684206369e2f
vitalyf2024 <162030022+vitalyf2024@users.noreply.github.com>
2024-03-17 17:41:12 +0200

    Added HW Task 2
    Added homework task 2 (5 user number inputs and the sum)

--- Day4_loops/src/Main.java
+++ Day4_loops/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        hw_1();
+        hw_2();
     }
 
     public static void task1(){
@@ -48,5 +48,17 @@ public class Main {
         } else {
             System.out.println("Ievadiet skaitli, kas ir lielāks nekā 0!");
         }
+    }
+
+    public static void hw_2() {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Ievadiet skaitli:");
+            int numb = sc.nextInt();
+            sum = sum + numb;
+        }
+        System.out.println("Jūsu skaitļu summa ir " + sum);
     }
 }
