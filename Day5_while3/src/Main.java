a255828d4733c97c4cbbec82ca7c10994fd9d1bf
vitalyf2024 <162030022+vitalyf2024@users.noreply.github.com>
2024-03-24 18:41:59 +0200

    Added Day5_while3
    Day5_while3 is allowing to run code but has hard time working with assigning variable "a" type String.

--- Day5_while3/src/Main.java
+++ Day5_while3/src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task0(){
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+
+        while(!input.equals("exit")) {
+            System.out.println("Ievadiet vardu!");
+            input = sc.nextLine();
+        }
+    }
+
+    public static void task1(){
+        Scanner sc = new Scanner(System.in);
+        java.lang.String a = "";
+        int sum = 0;
+
+        while(!a.equals("exit")) {
+            System.out.println("Ievadiet skaitli: ");
+            int number = sc.nextLine();
+            sum = sum + number;
+            System.out.println("Beigt ievadi?");
+            a = sc.nextLine();
+        }
+        System.out.println("Skaitļu summa ir " + sum);
     }
 }
