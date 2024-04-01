c6c547108518e996f91345b2ccf508c58a7102e7
vitalyf2024 <162030022+vitalyf2024@users.noreply.github.com>
2024-04-01 19:42:14 +0300

    Day6 added

---
+++ Day6_methods/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        task2(0);
+    }
+
+    public static void task1(String a, String b, String c) {
+        String result = a + b + c;
+        System.out.println(result);
+    }
+
+    public static void task2(int count) {
+
+        if (count > 1) {
+            int sum = 0;
+            for (int i = 0; i < count; i++) {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Ievadiet skaitli: ");
+
+                int number = sc.nextInt();
+                sum += number;
+            }
+            System.out.println("Skaitļu summa: " + sum);
+        } else {
+            System.out.println("ERROR: Count parameter less than 1");
+        }
+    }
+}
