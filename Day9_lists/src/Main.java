36790226f09510ba9deb0a9299a9397982347ee0
vitalyf2024 <162030022+vitalyf2024@users.noreply.github.com>
2024-04-17 20:09:23 +0300

    Day9 - Task2 added

--- Day9_lists/src/Main.java
+++ Day9_lists/src/Main.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int[] a = {1, 11, 111, 1111};
-        task1(a);
+        task2(a);
     }
 
     public static void task1(int[] a) {
         for (int i = 0; i < a.length; i++) {
             System.out.println(a[i]+"");
+        }
+    }
+    public static void task2(int[] a) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ievadiet skaitli: ");
+        int number = sc.nextInt();
+        boolean b = false;
+
+        for (int i = 0; i < a.length; i++) {
+            if (number == a[i]) {
+                System.out.println("Skaitļa indeks: "+ (i+1));
+                b = true;
+            }
+        }
+
+        if (b == false) {
+            System.out.println("Skaitlis "+number+" nav atrasts!");
         }
     }
 }
